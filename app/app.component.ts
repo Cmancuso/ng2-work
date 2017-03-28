a264aee99059a79eab1789a0c04cf7c2a02c954c
Cmancuso <Christopher.a.mancuso@gmail.com>
2017-03-28 16:54:07 -0400

    Add event emitter

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,22 @@
 import {Component, Input} from 'angular2/core';
 import {CoursesComponent} from './courses.component'
+import {FavoriteComponent} from './favorite.component'
 
 @Component({
     selector: 'my-app',
     template: `<h1>Hello Angular</h1>
-    <i (click) = "onclick($event)" [class]="isClicked ? 'glyphicon glyphicon-star' : 'glyphicon glyphicon-star-empty'"></i>
-    `
-
+				<favorite [isClicked] = "post.isClicked" (change) = onFavoriteChange($event)></favorite>
+				
+    			`,
+    directives: [FavoriteComponent]
 })
 export class AppComponent { 
-	@Input() isClicked = false;
-	onclick(){
-		this.isClicked = !this.isClicked
-		console.log(this.isClicked)
+	post = {
+		title: "this is the title",
+		isClicked: true
+	}
+
+	onFavoriteChange($event){
+		console.log($event)
 	}
 }
---
+++ app/favorite.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, EventEmitter, Output} from 'angular2/core';
+
+
+@Component({
+    selector: 'favorite',
+    template: `<i (click) = "onclick($event)" 
+                [class]="isClicked ? 'glyphicon glyphicon-star' 
+                : 'glyphicon glyphicon-star-empty'">
+                </i>
+            `
+})
+export class FavoriteComponent { 
+    
+	@Input() isClicked = false;
+    @Output() change = new EventEmitter
+
+	onclick(){
+		this.isClicked = !this.isClicked
+        this.change.emit({newvalue: this.isClicked})
+		console.log(this.isClicked)
+	}
+}
